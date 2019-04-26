070ae0aca803a7a61a5f0bb3ec93071a816090b3
flhorizonte <felipehorizonte@zettasolucoes.com.br>
2019-04-26 10:19:58 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+new Vue({
+    el: "#app",
+    data: {
+        //matriz 3x3
+        button: [
+            [
+                [{ status: true }], [{ status: true }], [{ status: true }]
+            ],
+            [
+                [{ status: true }], [{ status: true }], [{ status: true }]
+            ],
+            [
+                [{ status: true }], [{ status: true }], [{ status: true }]
+            ]
+        ],
+    },
+    methods: {
+        clicked(index, id) {
+            //DEBUG linhas e colunas da matriz
+            // console.log(`Linha => ${index} Coluna => ${id}`)
+            this.button[index][id][0].status = !this.button[index][id][0].status
+        }
+    },
+    mounted() {
+        //DEBUG matriz
+        console.log(this.button)
+    }
+})
