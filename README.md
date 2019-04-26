0a9f32a7d074aa92fe7a9ebe019dcd4f3b87d1d7
flhorizonte <felipehorizonte@zettasolucoes.com.br>
2019-04-26 15:28:31 -0300

    Game Finished

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# VUELHA CDN VERSION
+# VUELHA
 
-A tictactoe game made with vuejs
+A simple tictactoe in vuejs
+
+### Installing
+
+Cloning
+
+```
+$ git clone https://github.com/flhorizonte/VUELHA.git
+```
+
+## Built With
+
+* [Vue.js](https://vuejs.org/) - The javascript framework used
+* [Npm](https://www.npmjs.com/) - Dependency Management
+
+## Authors
+
+* **Billie Thompson** - *Initial work* - [flhorizonte](https://github.com/flhorizonte)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- src/vuelha.js
+++ src/vuelha.js
@@ -1,24 +1,166 @@
 const vuelha = new Vue({
     el: "#app",
     data: {
-        //matriz 3x3
         button: [
             [
-                [{ status: true }], [{ status: true }], [{ status: true }]
+                [{ status: Boolean }], [{ status: Boolean }], [{ status: Boolean }]
             ],
             [
-                [{ status: true }], [{ status: true }], [{ status: true }]
+                [{ status: Boolean }], [{ status: Boolean }], [{ status: Boolean }]
             ],
             [
-                [{ status: true }], [{ status: true }], [{ status: true }]
+                [{ status: Boolean }], [{ status: Boolean }], [{ status: Boolean }]
             ]
         ],
+        turno: 1
     },
     methods: {
         clicked(index, id) {
+            console.log(this.turno)
             //DEBUG linhas e colunas da matriz
             // console.log(`Linha => ${index} Coluna => ${id}`)
-            this.button[index][id][0].status = !this.button[index][id][0].status
+            this.button[index][id][0].status = (this.turno == 1) ? true : false
+
+            if (this.turno == 2) {
+                this.turno--
+            } else if (this.turno == 1) {
+                this.turno++
+            }
+
+            this.whoWin(function (winner) {
+                setTimeout(() => {
+                    let rodada = confirm(`${winner == 1 ? 'X' : 'O'} venceu!!`)
+                    if (rodada == true) window.location.reload()
+                }, 500)
+            })
+
+            console.log(this.button)
+        },
+
+        whoWin(callback) {
+            let winner = ''
+
+            // Linha horizontal 1
+            if (this.button[0][0][0].status == true
+                && this.button[0][1][0].status == true
+                && this.button[0][2][0].status == true) {
+
+                winner = 1
+                callback(winner)
+            } else if (this.button[0][0][0].status == false
+                && this.button[0][1][0].status == false
+                && this.button[0][2][0].status == false) {
+
+                winner = 2
+                callback(winner)
+            }
+
+            // Linha horizontal 2
+            if (this.button[1][0][0].status == true
+                && this.button[1][1][0].status == true
+                && this.button[1][2][0].status == true) {
+
+                winner = 1
+                callback(winner)
+            } else if (this.button[1][0][0].status == false
+                && this.button[1][1][0].status == false
+                && this.button[1][2][0].status == false) {
+
+                winner = 2
+                callback(winner)
+            }
+
+            // Linha horizontal 3
+            if (this.button[2][0][0].status == true
+                && this.button[2][1][0].status == true
+                && this.button[2][2][0].status == true) {
+
+                winner = 1
+                callback(winner)
+            } else if (this.button[2][0][0].status == false
+                && this.button[2][1][0].status == false
+                && this.button[2][2][0].status == false) {
+
+                winner = 2
+                callback(winner)
+            }
+
+            // Linha vertical 1
+            if (this.button[0][0][0].status == true
+                && this.button[1][0][0].status == true
+                && this.button[2][0][0].status == true) {
+
+                winner = 1
+                callback(winner)
+            } else if (this.button[0][0][0].status == false
+                && this.button[1][0][0].status == false
+                && this.button[2][0][0].status == false) {
+
+                winner = 2
+                callback(winner)
+            }
+
+            // Linha vertical 2
+            if (this.button[0][1][0].status == true
+                && this.button[1][1][0].status == true
+                && this.button[2][1][0].status == true) {
+
+                winner = 1
+                callback(winner)
+            } else if (this.button[0][1][0].status == false
+                && this.button[1][1][0].status == false
+                && this.button[2][1][0].status == false) {
+
+                winner = 2
+                callback(winner)
+            }
+
+            //Linha vertical 3
+            if (this.button[0][2][0].status == true
+                && this.button[1][2][0].status == true
+                && this.button[2][2][0].status == true) {
+
+                winner = 1
+                callback(winner)
+            } else if (this.button[0][2][0].status == false
+                && this.button[1][2][0].status == false
+                && this.button[2][2][0].status == false) {
+
+                winner = 2
+                callback(winner)
+            }
+
+            //Linha diagonal 1
+            if (this.button[0][0][0].status == true
+                && this.button[1][1][0].status == true
+                && this.button[2][2][0].status == true) {
+
+                winner = 1
+                callback(winner)
+            } else if (this.button[0][0][0].status == false
+                && this.button[1][1][0].status == false
+                && this.button[2][2][0].status == false) {
+
+                winner = 2
+                callback(winner)
+            }
+
+
+            //Linha diagonal 2
+            if (this.button[0][2][0].status == true
+                && this.button[1][1][0].status == true
+                && this.button[2][0][0].status == true) {
+
+                winner = 1
+                callback(winner)
+            } else if (this.button[0][2][0].status == false
+                && this.button[1][1][0].status == false
+                && this.button[2][0][0].status == false) {
+
+                winner = 2
+                callback(winner)
+            }
+
         }
     },
     mounted() {
