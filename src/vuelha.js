cc89098e55b9bee71a966fa8914d681de277e749
flhorizonte <felipehorizonte@zettasolucoes.com.br>
2019-04-29 11:28:08 -0300

    Bug/ninguemGanhou Fixed

--- src/vuelha.js
+++ src/vuelha.js
@@ -2,169 +2,92 @@ const vuelha = new Vue({
     el: "#app",
     data: {
         button: [
-            [
-                [{ status: Boolean }], [{ status: Boolean }], [{ status: Boolean }]
-            ],
-            [
-                [{ status: Boolean }], [{ status: Boolean }], [{ status: Boolean }]
-            ],
-            [
-                [{ status: Boolean }], [{ status: Boolean }], [{ status: Boolean }]
-            ]
+            [[{ status: Boolean }], [{ status: Boolean }], [{ status: Boolean }]],
+            [[{ status: Boolean }], [{ status: Boolean }], [{ status: Boolean }]],
+            [[{ status: Boolean }], [{ status: Boolean }], [{ status: Boolean }]]
         ],
-        turno: 1
+        turno: 1,
+        jogadas: 0
     },
     methods: {
         clicked(index, id) {
-            console.log(this.turno)
-            //DEBUG linhas e colunas da matriz
-            // console.log(`Linha => ${index} Coluna => ${id}`)
-            this.button[index][id][0].status = (this.turno == 1) ? true : false
-
+            console.log(this.jogadas)
+            this.button[index][id][0].status = this.turno == 1 ? true : false;
             if (this.turno == 2) {
-                this.turno--
+                this.turno--;
             } else if (this.turno == 1) {
-                this.turno++
+                this.turno++;
             }
-
             this.whoWin(function (winner) {
                 setTimeout(() => {
-                    let rodada = confirm(`${winner == 1 ? 'X' : 'O'} venceu!!`)
-                    if (rodada == true) window.location.reload()
-                }, 500)
-            })
-
-            console.log(this.button)
+                    let rodada = confirm(
+                        `${winner == 0 ? "Ninguém" : winner == 1 ? "X" : "O"} venceu!!`
+                    );
+                    if (rodada == true) window.location.reload();
+                }, 500);
+            });
+            this.jogadas++;
         },
 
         whoWin(callback) {
-            let winner = ''
-
-            // Linha horizontal 1
-            if (this.button[0][0][0].status == true
-                && this.button[0][1][0].status == true
-                && this.button[0][2][0].status == true) {
-
-                winner = 1
-                callback(winner)
-            } else if (this.button[0][0][0].status == false
-                && this.button[0][1][0].status == false
-                && this.button[0][2][0].status == false) {
-
-                winner = 2
-                callback(winner)
+            if (
+                (this.button[0][0][0].status == true &&
+                    this.button[0][1][0].status == true &&
+                    this.button[0][2][0].status == true) ||
+                (this.button[1][0][0].status == true &&
+                    this.button[1][1][0].status == true &&
+                    this.button[1][2][0].status == true) ||
+                (this.button[2][0][0].status == true &&
+                    this.button[2][1][0].status == true &&
+                    this.button[2][2][0].status == true) ||
+                (this.button[0][0][0].status == true &&
+                    this.button[1][0][0].status == true &&
+                    this.button[2][0][0].status == true) ||
+                (this.button[0][1][0].status == true &&
+                    this.button[1][1][0].status == true &&
+                    this.button[2][1][0].status == true) ||
+                (this.button[0][0][0].status == true &&
+                    this.button[1][1][0].status == true &&
+                    this.button[2][2][0].status == true) ||
+                (this.button[0][2][0].status == true &&
+                    this.button[1][1][0].status == true &&
+                    this.button[2][0][0].status == true)
+            ) {
+                callback(1);
+            } else if (
+                (this.button[0][0][0].status == false &&
+                    this.button[0][1][0].status == false &&
+                    this.button[0][2][0].status == false) ||
+                (this.button[1][0][0].status == false &&
+                    this.button[1][1][0].status == false &&
+                    this.button[1][2][0].status == false) ||
+                (this.button[2][0][0].status == false &&
+                    this.button[2][1][0].status == false &&
+                    this.button[2][2][0].status == false) ||
+                (this.button[0][0][0].status == false &&
+                    this.button[1][0][0].status == false &&
+                    this.button[2][0][0].status == false) ||
+                (this.button[0][1][0].status == false &&
+                    this.button[1][1][0].status == false &&
+                    this.button[2][1][0].status == false) ||
+                (this.button[0][2][0].status == false &&
+                    this.button[1][2][0].status == false &&
+                    this.button[2][2][0].status == false) ||
+                (this.button[0][0][0].status == false &&
+                    this.button[1][1][0].status == false &&
+                    this.button[2][2][0].status == false) ||
+                (this.button[0][2][0].status == false &&
+                    this.button[1][1][0].status == false &&
+                    this.button[2][0][0].status == false)
+            ) {
+                callback(2);
+            } else if (this.jogadas >= 8) {
+                callback(0);
             }
-
-            // Linha horizontal 2
-            if (this.button[1][0][0].status == true
-                && this.button[1][1][0].status == true
-                && this.button[1][2][0].status == true) {
-
-                winner = 1
-                callback(winner)
-            } else if (this.button[1][0][0].status == false
-                && this.button[1][1][0].status == false
-                && this.button[1][2][0].status == false) {
-
-                winner = 2
-                callback(winner)
-            }
-
-            // Linha horizontal 3
-            if (this.button[2][0][0].status == true
-                && this.button[2][1][0].status == true
-                && this.button[2][2][0].status == true) {
-
-                winner = 1
-                callback(winner)
-            } else if (this.button[2][0][0].status == false
-                && this.button[2][1][0].status == false
-                && this.button[2][2][0].status == false) {
-
-                winner = 2
-                callback(winner)
-            }
-
-            // Linha vertical 1
-            if (this.button[0][0][0].status == true
-                && this.button[1][0][0].status == true
-                && this.button[2][0][0].status == true) {
-
-                winner = 1
-                callback(winner)
-            } else if (this.button[0][0][0].status == false
-                && this.button[1][0][0].status == false
-                && this.button[2][0][0].status == false) {
-
-                winner = 2
-                callback(winner)
-            }
-
-            // Linha vertical 2
-            if (this.button[0][1][0].status == true
-                && this.button[1][1][0].status == true
-                && this.button[2][1][0].status == true) {
-
-                winner = 1
-                callback(winner)
-            } else if (this.button[0][1][0].status == false
-                && this.button[1][1][0].status == false
-                && this.button[2][1][0].status == false) {
-
-                winner = 2
-                callback(winner)
-            }
-
-            //Linha vertical 3
-            if (this.button[0][2][0].status == true
-                && this.button[1][2][0].status == true
-                && this.button[2][2][0].status == true) {
-
-                winner = 1
-                callback(winner)
-            } else if (this.button[0][2][0].status == false
-                && this.button[1][2][0].status == false
-                && this.button[2][2][0].status == false) {
-
-                winner = 2
-                callback(winner)
-            }
-
-            //Linha diagonal 1
-            if (this.button[0][0][0].status == true
-                && this.button[1][1][0].status == true
-                && this.button[2][2][0].status == true) {
-
-                winner = 1
-                callback(winner)
-            } else if (this.button[0][0][0].status == false
-                && this.button[1][1][0].status == false
-                && this.button[2][2][0].status == false) {
-
-                winner = 2
-                callback(winner)
-            }
-
-
-            //Linha diagonal 2
-            if (this.button[0][2][0].status == true
-                && this.button[1][1][0].status == true
-                && this.button[2][0][0].status == true) {
-
-                winner = 1
-                callback(winner)
-            } else if (this.button[0][2][0].status == false
-                && this.button[1][1][0].status == false
-                && this.button[2][0][0].status == false) {
-
-                winner = 2
-                callback(winner)
-            }
-
         }
     },
     mounted() {
         //DEBUG matriz
-        console.log(this.button)
+        console.log(this.button);
     }
-})
+});
